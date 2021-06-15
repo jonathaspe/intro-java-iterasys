2ae06c3060ebde93fcbc52f30cbb5f201eddd3ef
jonathaspe <jonathaspe@hotmail.co>
2021-06-15 00:51:30 -0300

    Exclusão do "Bom dia" na impressão do Console;
    Criação dos métodos calcularAreaCurto e alcularAreaExtenso;
    
    O método main agora chamam esses outros dois métodos static void

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -1,6 +1,6 @@
 //Exemplo de uma loja de construção
 
-//1 - pacote
+//1 - Pacote
 package intro;
 
 //2 - Referência às bibliotecas
@@ -13,21 +13,39 @@ public class Medidas {
 
     //3.2 - Métodos e funções (o que a coisa sabe fazer)
     public static void main(String[] args){
-        System.out.println("Bom dia!");
-        System.out.println("Cálculo da área do tapete:");
+        calcularAreaCurto();
+        calcularAreaExtenso();
+
+    }
+
+    public static void calcularAreaCurto(){
+        System.out.println("Cálculo da área Modo Curto:");
 
         //Cálculo de área, exemplo: tamanho do tapete ou do piso
         //Variáveis
-        int largura;
+        int largura = 4; // largura recebe 4
+        int comprimento = 3;
+
+        System.out.println("Para o largura de " + largura + "m e o comprimento de "
+                + comprimento + "m, a área do tapete é de " + largura * comprimento + "m²");
+    }
+
+    public static void calcularAreaExtenso(){
+        System.out.println("Cálculo da área Modo Extenso:");
+
+        //Cálculo de área, exemplo: tamanho do tapete ou do piso
+        //Variáveis
+        int largura; // largura recebe 4
         int comprimento;
         int resultado;
 
         //Valores
-        largura = 4; // largura recebe 4
-        comprimento = 3;
-
+        largura = 5;
+        comprimento = 6;
         resultado = largura * comprimento;
+
         System.out.println("Para o largura de " + largura + "m e o comprimento de "
                 + comprimento + "m, a área do tapete é de " + resultado + "m²");
     }
+
 }
